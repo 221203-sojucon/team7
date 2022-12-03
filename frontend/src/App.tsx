1e6c217c25bdf19c0c344e37f208743c7577211c
KumJungMin <rmawjdals@naver.com>
2022-12-03 18:22:22 +0900

    chore: add common style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,21 @@ import { RoomPage } from "./components/room";
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/room" element={<RoomPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div className="App" style={{
+          height: '100vh'
+				}}>
+      <section style={{
+          margin: '0 auto',
+          width: '390px',
+          height: '844px',
+				}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />}></Route>
+            <Route path="/room" element={<RoomPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </section>
     </div>
   );
 };
