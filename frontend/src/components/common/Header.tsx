a43e5f3898a992a32f33e605a52c4b348d7eef62
KumJungMin <rmawjdals@naver.com>
2022-12-03 17:15:34 +0900

    chore: add Header common component

---
+++ frontend/src/components/common/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "./Header.module.scss";
+
+type HeaderType = {
+  hasShareButton?: boolean;
+  children: React.ReactNode;
+};
+
+export const Header = ({ children }: HeaderType) => {
+  return (
+    <header className={ style.Header }>
+      <h1 className={ style.Title }>{children}</h1>
+      <button className={ style.BtnShare } onClick={ CopyUrlToClipboard }>
+        <div className={ style.ShareImg }></div>
+        <span className={ style.Label }>공유</span>
+      </button>
+    </header>
+  )
+};
+
+Header.defaultProps = {
+  hasShareButton: true,
+  children: "",
+};
+  
+function CopyUrlToClipboard(){
+  navigator.clipboard.writeText(window.location.href);
+}
