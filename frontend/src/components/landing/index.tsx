ed736caef5fac1abfa138f49e6afb952f0b51db5
KumJungMin <rmawjdals@naver.com>
2022-12-03 19:08:11 +0900

    chore: landing

--- frontend/src/components/landing/index.tsx
+++ frontend/src/components/landing/index.tsx
@@ -7,8 +7,9 @@ export const LandingPage = () => {
   return (
     <div className={ style.Landing }>
       <img className={ style.Image }></img>
-      <img className={ style.ImageText }></img>
-      <Button color="light">특별한 크리스마스 만들기</Button>
+      <div className={ style.Btn }>
+        <Button>특별한 크리스마스 만들기</Button>
+      </div>
     </div>
   );
 };
