0eb3758ad12c726a3ea27f8a84fd284d201228be
seohyun319 <tjguselly0319@gmail.com>
2022-12-03 15:43:59 +0900

    [feat] set router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>{/* <Route path="/" element={<MainPage />}></Route> */}</Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
