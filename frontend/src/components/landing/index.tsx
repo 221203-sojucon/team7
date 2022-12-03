e0582f64fee1a0990e9fa4a9f140205ddb3b58ef
KumJungMin <rmawjdals@naver.com>
2022-12-03 18:58:32 +0900

    chore landing image

--- frontend/src/components/landing/index.tsx
+++ frontend/src/components/landing/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+
 import { Button } from "../common/Button";
+import style from './Landing.module.scss'
 
 export const LandingPage = () => {
   return (
-    <div>
-      <div>얼레벌레 크리스마스 보내기!</div>
-      <div>특별한 크리스마스를 보내고 싶으신가요? 친구들에게 요청해보세요.</div>
+    <div className={ style.Landing }>
+      <img className={ style.Image }></img>
+      <img className={ style.ImageText }></img>
       <Button color="light">특별한 크리스마스 만들기</Button>
     </div>
   );
